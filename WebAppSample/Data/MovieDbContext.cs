8db955b81f3c045bb69286fec3fb1497fc7899d9
Hashimoto-Teppei <hashimoto@tnt-info.com>
2023-01-31 18:02:22 +0900

    add Movies page

---
+++ WebAppSample/Data/MovieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebAppSample.Models;
+
+namespace WebAppSample.Data
+{
+	public class MovieDbContext: DbContext
+	{
+		public MovieDbContext(DbContextOptions<MovieDbContext> options): base(options)
+		{
+		}
+
+		public DbSet<Movie> Movie => Set<Movie>();
+	}
+}
+
